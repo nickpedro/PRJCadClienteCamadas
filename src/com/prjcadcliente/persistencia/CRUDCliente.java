cf51d4ec2a2c53ca69c02a6e7a5b2ce4af84ad60
pedro <pedrosalomaoventura1516@gmail.com>
2019-05-14 17:13:55 -0300

    adicionamos o corpo do cadastro

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -1,5 +1,10 @@
 package com.prjcadcliente.persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.prjcadcliente.dominio.Cliente;
@@ -18,8 +23,56 @@ import com.prjcadcliente.dominio.Cliente;
  *
  */
 public class CRUDCliente {
+	
+	//Declaração das instâncias de comunicação com o banco
+	//de dados
+	private Connection con = null;
+	private ResultSet rs = null;
+	private PreparedStatement pst = null;
+	
 	public String cadastrar(Cliente cliente) {
-			return null;
+		
+		String msg = "";
+		
+		
+		//Criação dos objetos para a conexão com o banco de dados
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			
+			String consulta = "INSERT INTO tbcliente(nome,email,telefone,idade)values(?,?,?,?)";
+					
+			pst = con.prepareStatement(consulta);
+			
+			pst.setString(1, cliente.getNome());
+			pst.setString(2, cliente.getEmail());
+			pst.setString(3, cliente.getTelefone());
+			pst.setInt(4, cliente.getIdade());
+			
+			int r = pst.executeUpdate();
+			
+			;
+			if (r > 0)
+				msg = "Cadastro realizado com sucesso!";
+			else
+				msg = "Não foi possivel realizar o cadastro!";
+			
+			
+			
+		}
+		catch(SQLException ex) {
+			msg = "Erro ao tentar cadastrar: "+ex.getMessage();
+		}
+		catch(Exception e) {
+			msg = "Erro inesperado: "+e.getMessage();
+			
+		}
+		finally {
+			try{con.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		
+		return msg;
+		
 		}
 	public String atualizar(Cliente cliente) {
 		return null;
