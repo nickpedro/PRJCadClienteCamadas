82ee77301b3a76728ecccef9609aacfe31c2589a
pedro <pedrosalomaoventura1516@gmail.com>
2019-05-14 17:23:53 -0300

    Criação de cargo de método Atualizar

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -75,7 +75,47 @@ public class CRUDCliente {
 		
 		}
 	public String atualizar(Cliente cliente) {
-		return null;
+String msg = "";
+		
+		
+		//Criação dos objetos para a conexão com o banco de dados
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			
+			String consulta = "UPDATE tbcliente SET nome=?,email=?,telefone=?,idade=? WHERE id=?";
+					
+			pst = con.prepareStatement(consulta);
+			
+			pst.setString(1, cliente.getNome());
+			pst.setString(2, cliente.getEmail());
+			pst.setString(3, cliente.getTelefone());
+			pst.setInt(4, cliente.getIdade());
+			pst.setInt(5, cliente.getId());
+			
+			int r = pst.executeUpdate();
+			
+			;
+			if (r > 0)
+				msg = "Atualização realizada com sucesso!";
+			else
+				msg = "Não foi possivel realizar a atualização!";
+			
+			
+			
+		}
+		catch(SQLException ex) {
+			msg = "Erro ao tentar atualizar: "+ex.getMessage();
+		}
+		catch(Exception e) {
+			msg = "Erro inesperado: "+e.getMessage();
+			
+		}
+		finally {
+			try{con.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		
+		return msg;
 	}
 	public String deletar(Cliente cliente) {
 		return null;
